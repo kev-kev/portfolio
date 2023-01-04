1fbec5190e41eea3e8ba60f4bf47214f1d771087
kev-kev <kevin.clark93@gmail.com>
2023-01-04 10:23:21 -0500

    Add card bar headers

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+// create project cards
 for(let i = 0; i < 4; i++) {
   const projectCard = document.createElement('div');
   const title = document.createElement('h2');
@@ -9,4 +10,22 @@ for(let i = 0; i < 4; i++) {
     ? projectCard.setAttribute('class', 'card project-card l-offset')
     : projectCard.setAttribute('class', 'card project-card r-offset')
   document.getElementById('project-card-container').append(projectCard);
+}
+
+// add header bars to cards
+const barColors = {
+  blue: '#82B4FF',
+  red: '#FF9587',
+  green: '#97DF5F',
+  yellow: '#FFE587',
+  pink: '#FF93F4',
+  black: '#1B1B1B',
+};
+const cards = document.getElementsByClassName('card');
+const colorValues = Object.values(barColors);
+for(const card of cards) {
+  const bar = document.createElement('div');
+  bar.setAttribute('class', 'card-header-bar');
+  bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)]
+  card.prepend(bar);
 }
