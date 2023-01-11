c98b23bc1839e333e08ab74bb57794bc54ce9e86
kev-kev <kevin.clark93@gmail.com>
2023-01-11 15:03:48 -0500

    Move divs from js to HTML, run JS on window load, adjusting grid spacing

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,32 @@
-// create project cards
-for(let i = 0; i < 4; i++) {
-  const projectCard = document.createElement('div');
-  const contentContainer = document.createElement('div');
-  contentContainer.setAttribute('class', 'card-content');
-  const title = document.createElement('h2');
-  title.append(document.createTextNode('Lorem ipsum dolor'));
-  const description = document.createElement('p');
-  description.append(document.createTextNode('Sit amet consectetur adipisicing elit. Doloremque odio harum alias magni fugit cum, saepe nesciunt pariatur! Consequatur accusantium dolore animi iusto et perspiciatis ex totam a nam dolores?'));
-  contentContainer.append(title, description);
-  projectCard.append(contentContainer);
-  i % 2 === 0 
-    ? projectCard.setAttribute('class', 'card project-card l-offset')
-    : projectCard.setAttribute('class', 'card project-card r-offset')
-  console.log(projectCard)
-  document.getElementById('project-card-container').append(projectCard);
-}
-
-// add header bars to cards
 const barColors = {
   blue: '#82B4FF',
   red: '#FF9587',
   green: '#97DF5F',
   yellow: '#FFE587',
   pink: '#FF93F4',
-  // black: '#1B1B1B',
 };
 
-const cards = document.getElementsByClassName('card');
 const colorValues = Object.values(barColors);
-for(const card of cards) {
-  const bar = document.createElement('div');
-  bar.setAttribute('class', 'card-header-bar');
-  bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)]
-  card.prepend(bar);
-}
+
+window.addEventListener("load", () => { 
+  const bars = document.getElementsByClassName('card-bar');
+  for(const bar of bars) {
+    bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)];
+  }
+  
+  const iconContainers = document.getElementsByClassName('card-bar-icon-container')
+  for(const iconContainer of iconContainers) {
+    iconContainer.innerHTML = `
+      <i class="bar-icon fa-solid fa-xmark"></i>
+      <i class="bar-icon fa-regular fa-circle"></i>
+      <i class="bar-icon fa-solid fa-down-left-and-up-right-to-center"></i>
+    `;
+  }
+  
+  [...document.getElementsByClassName('project-card-content')].forEach((content) => {
+    content.innerHTML = `
+      <h2 class="card-title">Lorem ipsum dolor</h2>
+      <p class="card-description">Sit amet consectetur adipisicing elit. Doloremque odio harum alias magni fugit cum, saepe nesciunt pariatur! Consequatur accusantium dolore animi iusto et perspiciatis ex totam a nam dolores?</p>
+    `
+  })
+});
