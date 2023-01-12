d1accb410e68345409b7096818b31e5ccc9e5633
kev-kev <kevin.clark93@gmail.com>
2023-01-12 15:23:32 -0500

    Add titles to card bars

--- src/scripts.js
+++ src/scripts.js
@@ -8,8 +8,8 @@ const barColors = {
   pink: '#FF93F4',
 };
 
-const colorValues = Object.values(barColors);
 window.addEventListener("load", () => { 
+  // create project cards
   const projectCardContainer = document.getElementById('project-card-container');
   for(let i = 0; i < projectData.length; i++) {
     const projectCard = document.createElement('div');
@@ -24,28 +24,44 @@ window.addEventListener("load", () => {
     projectCard.append(cardBar, content);
     projectCardContainer.append(projectCard);
   }
-  
+
   let idx = 0;
   [...document.getElementsByClassName('project-card-content')].forEach((content) => {
+    // debugger
     content.innerHTML = `
       <div class="project-card-img" style="background-image: url('${projectData[idx].imagePath}');"></div>
-      <h2 class="card-title">${projectData[idx].name}</h2>
-      <p class="card-description">${projectData[idx].notes}</p>
+      <div class="project-card-text-container">
+        <h2 class="project-card-title">${projectData[idx].name}</h2>
+        <p class="project-card-description">${projectData[idx].notes}</p>
+        <a href='${projectData[idx].url}' target="_blank"><button class="btn">VISIT PROJECT</button></a>
+      </div>
     `
+
+    const cardBarTitle = document.createElement('span');
+    cardBarTitle.setAttribute('class', 'card-bar-title')
+    cardBarTitle.textContent = projectData[idx].name
+    content.previousElementSibling.prepend(cardBarTitle);
+
     idx++;
   })
 
-  const bars = document.getElementsByClassName('card-bar');
-  for(const bar of bars) {
+  // set bar colors
+  const colorValues = Object.values(barColors);
+  [...document.getElementsByClassName('card-bar')].forEach((bar) => {
     bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)];
-  }
+  })
   
+  // add fa icons
   const iconContainers = document.getElementsByClassName('card-bar-icon-container')
   for(const iconContainer of iconContainers) {
     iconContainer.innerHTML = `
-      <i class="bar-icon fa-solid fa-xmark"></i>
-      <i class="bar-icon fa-regular fa-circle"></i>
-      <i class="bar-icon fa-solid fa-down-left-and-up-right-to-center"></i>
+    <i class="bar-icon fa-solid fa-down-left-and-up-right-to-center"></i>
+    <i class="bar-icon fa-regular fa-circle"></i>
+    <i class="bar-icon fa-solid fa-xmark"></i>
     `;
   }
+
+  // [...document.getElementsByClassName('project-card')].forEach((card) => {
+  //   debugger
+  // })
 });
