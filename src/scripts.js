4e5d68be24c58f6fb55aeb6af0d5ae1edb24e777
kev-kev <kevin.clark93@gmail.com>
2023-01-18 15:43:16 -0500

    Functionality for fwd/back buttons, moving styling in js to top of page that was lagging behind yt loading

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,22 @@ const barColors = {
   pink: '#FF93F4',
 };
 
+// set bar colors
+const colorValues = Object.values(barColors);
+[...document.getElementsByClassName('card-bar')].forEach((bar) => {
+  bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)];
+})
+
+// add fa icons
+const iconContainers = document.getElementsByClassName('card-bar-icon-container')
+for(const iconContainer of iconContainers) {
+  iconContainer.innerHTML = `
+  <i class="bar-icon fa-solid fa-down-left-and-up-right-to-center"></i>
+  <i class="bar-icon fa-regular fa-circle"></i>
+  <i class="bar-icon fa-solid fa-xmark"></i>
+  `;
+}
+
 const songData = [
   {
     title: 'Skylar Spence',
@@ -48,54 +64,20 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onPlayerReady(event) {
-  // add event listeners to player buttons
-  document.querySelector(".play-btn").addEventListener("click", () => {
-    event.target.playVideo();
-  });
-  document.querySelector(".fa-circle-pause").addEventListener("click", () => {
-    pauseVideo(); 
-  })
-}
-
-let done = false;
-function onPlayerStateChange(event) {
-  console.log("player state changed to", event.data);
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    console.log("Going to stop video in 6 secs");
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
-function pauseVideo() {
-  player.pauseVideo();
-}
-
-function stopVideo() {
-  player.stopVideo();
-}
-
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '0',
     width: '0',
-    videoId: 'y-7qbdxIPWc',
+    videoId: songData[0].id,
     playerVars: {
       'playsinline': 1
     },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
   });
 }
-
 window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
 
 window.addEventListener("load", () => { 
-
   // create project cards
   const projectCardContainer = document.getElementById('project-card-container');
   for(let i = 0; i < projectData.length; i++) {
@@ -114,7 +96,6 @@ window.addEventListener("load", () => {
 
   let idx = 0;
   [...document.getElementsByClassName('project-card-content')].forEach((content) => {
-    // debugger
     content.innerHTML = `
       <div class="project-card-img" style="background-image: url('${projectData[idx].imagePath}');"></div>
       <div class="project-card-text-container">
@@ -132,22 +113,6 @@ window.addEventListener("load", () => {
     idx++;
   })
 
-  // set bar colors
-  const colorValues = Object.values(barColors);
-  [...document.getElementsByClassName('card-bar')].forEach((bar) => {
-    bar.style.backgroundColor = colorValues[Math.floor(Math.random()*colorValues.length)];
-  })
-  
-  // add fa icons
-  const iconContainers = document.getElementsByClassName('card-bar-icon-container')
-  for(const iconContainer of iconContainers) {
-    iconContainer.innerHTML = `
-    <i class="bar-icon fa-solid fa-down-left-and-up-right-to-center"></i>
-    <i class="bar-icon fa-regular fa-circle"></i>
-    <i class="bar-icon fa-solid fa-xmark"></i>
-    `;
-  }
-
   document.getElementsByClassName('wavy-line-divider-container')[0].innerHTML = `
     <svg width="254" height="24" viewBox="0 0 254 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M2 12C10.3333 -1.33333 18.6667 -1.33333 27 12C35.3333 25.3333 43.6667 25.3333 52 12C60.3333 -1.33333 68.6667 -1.33333 77 12C85.3333 25.3333 93.6667 25.3333 102 12C110.333 -1.33333 118.667 -1.33333 127 12C135.333 25.3333 143.667 25.3333 152 12C160.333 -1.33333 168.667 -1.33333 177 12C185.333 25.3333 193.667 25.3333 202 12C210.333 -1.33333 218.667 -1.33333 227 12C235.333 25.3333 243.667 25.3333 252 12" stroke="black" stroke-width="3"/>
@@ -159,6 +124,27 @@ window.addEventListener("load", () => {
 
   typeWriter(document.getElementsByClassName('typewriter-text')[0], "Hey, I'm Kevin!");
 
+  const setSongData = () => {
+    document.querySelector('.music-card-song').innerHTML = songData[songIndex].title;
+    document.querySelector('.music-card-artist').innerHTML = songData[songIndex].artist;
+    document.getElementById('yt-link').setAttribute('href', `https://www.youtube.com/watch?v=${songData[songIndex].id}`)
+  }
+
+  const changeSong = () => {
+    player.cueVideoById(songData[songIndex].id);
+    player.nextVideo()
+    setSongData();
+  } 
+
+  setSongData();
+
+  document.querySelector(".play-btn").addEventListener("click", () => {
+    player.playVideo();
+  });
+  document.querySelector(".pause-btn").addEventListener("click", () => {
+    player.pauseVideo();
+  });
+  
   const play = document.querySelector('.play-btn');
   const pause = document.querySelector('.pause-btn');
   pause.setAttribute('style', 'display: none;');
@@ -171,10 +157,17 @@ window.addEventListener("load", () => {
       pause.setAttribute('style', 'display: none;');
     }
   })
-
-  // when clicking on fwd / back buttons, stop current song, increment songIndex, and play next song
-
-  document.querySelector('.music-card-song').innerHTML = songData[songIndex].title;
-  document.querySelector('.music-card-artist').innerHTML = songData[songIndex].artist;
-  document.getElementById('yt-link').setAttribute('href', `https://www.youtube.com/watch?v=${songData[songIndex].id}`)
+  
+  // when clicking on fwd / back buttons, cueVideoById(id, startSeconds)
+  document.querySelector('.back-btn').addEventListener('click', () => {
+    songIndex--;
+    if(songIndex === -1) songIndex = songData.length - 1;
+    changeSong();
+  })
+  document.querySelector('.fwd-btn').addEventListener('click', () => {
+    songIndex++;
+    if(songIndex === songData.length) songIndex = 0;
+    changeSong();
+  })
 });
+
