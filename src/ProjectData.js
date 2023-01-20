31a463dbb3eaaacef76e81318dc7325afeeea6b3
kev-kev <kevin.clark93@gmail.com>
2023-01-20 12:36:14 -0500

    Set breakpoints for main content width and padding

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -5,14 +5,19 @@ const projectData = [
     notes:
       "Web app to help you and your partner plan meals. Frontend: React with context and Material-UI, Backend: Ruby on Rails",
     imagePath: "./images/letsEat.png",
-    inProgress: false,
+  },
+  {
+    name: "Restaurant LP",
+    url: "https://github.com/kev-kev/restaurant-lp",
+    notes:
+      "A responsive landing page for a fictional restaurant with a dynamic menu and reservation form. Built with vanilla JavaScript.",
+    imagePath: "./images/restaurantLP.png"
   },
   {
     name: "Holler",
     url: "https://github.com/kev-kev/holler-app",
     notes: "Twitter clone made with Ruby on Rails and Bootstrap",
     imagePath: "./images/holler.png",
-    inProgress: false
   },
   {
     name: "Verbose",
@@ -20,7 +25,6 @@ const projectData = [
     notes:
       "Vocabulary learning app built with React Bootstrap and the Owlbot API",
     imagePath: "./images/verbose.png",
-    inProgress: false,
   },
   {
     name: "Salad Bowl",
@@ -28,14 +32,12 @@ const projectData = [
     notes:
       "Online multiplayer game made using Websockets and React. Submit words, act them out, and guess them!",
     imagePath: "./images/saladBowl.png",
-    inProgress: true,
   },
   {
     name: "lunchtime",
     url: "https://github.com/kev-kev/lunchtime",
     notes: "Top-down pixel shooter game, made with Phaser + ES6 + Webpack",
     imagePath: "./images/lunchTime.png",
-    inProgress: true,
   },
   {
     name: "Conway's Game of Life",
@@ -43,14 +45,12 @@ const projectData = [
     notes:
       "An implementation of Conway's Game of Life. Created using HTML5 Canvas",
     imagePath: "./images/gameOfLife.png",
-    inProgress: false,
   },
   {
     name: "MyIslandTracker",
     url: "https://github.com/kev-kev/acnh_tracker",
     notes: "A tracking tool for the video game Animal Crossing New Horizons.",
     imagePath: "./images/acnhTracker.png",
-    inProgress: false,
     walkthrough:
       "https://www.notion.so/kevkev/Animal-Crossing-Tracker-a4c84c1041544c2486799b86a85ec6a8",
   },
@@ -60,7 +60,6 @@ const projectData = [
     notes:
       "Browser-based drum machine made with React and the Web Drum Sequencer library.",
     imagePath: "./images/drumblr.png",
-    inProgress: false,
   },
   {
     name: "Super Pet Bros",
@@ -68,7 +67,6 @@ const projectData = [
     notes:
       "Create your very own virtual pet and battle it against others. Vanilla JS frontend and Ruby on Rails backend",
     imagePath: "./images/superPetBros.png",
-    inProgress: false,
   },
 ];
 
